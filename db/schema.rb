249e374d391f9496439fcb566d73b50f42873898
kato <i-kato@creationline.com>
2021-09-06 01:28:25 +0900

    use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_05_131459) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "image"
