6b883229caf7c869616a14c77ff2b051e893a02b
kato <i-kato@creationline.com>
2021-09-05 22:28:59 +0900

    add Item with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_05_131459) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
