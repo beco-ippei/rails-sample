041c934a94640a307bfd3600d59b1c97a8333cc2
kato <i-kato@creationline.com>
2021-09-07 00:19:19 +0900

    add deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/var/www/rails-sample"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "config/master.key"
+append :linked_files, "config/master.key", 'puma.rb'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
