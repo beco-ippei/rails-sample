2351e7ea524a6b3da14a76e9addfd27644bf17c1
kato <i-kato@creationline.com>
2021-09-07 01:46:00 +0900

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: redirect('/items')
   resources :items
+  get 'pages', to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
